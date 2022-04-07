7b73a4bbb43e7b4453b0d3388ce7949e9723f6ca
Vincent Lin <tdplayer24@gmail.com>
2022-04-07 12:52:12 -0500

    finished addactivitytoroutines

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -3,12 +3,12 @@ const client = require("./client");
 const addActivityToRoutine = async ({ routineId, activityId, count, duration }) => {
 
     try {
-        const { rows } = await client.query(`
+        const { rows: [activity] } = await client.query(`
         INSERT INTO routine_activities ("routineId", "activityId", count, duration)
         VALUES ($1, $2, $3, $4)
+        RETURNING *
     `, [routineId, activityId, count, duration])
-
-        return rows
+        return activity
     } catch (error) {
         throw error
     }
