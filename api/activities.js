b642915d064abe152e47bc94b1a1792f7afd09b2
Taylor Kirkes <kirkes21@gmail.com>
2022-04-09 18:54:14 -0500

    finish post route to create activity, start route to update activity

--- api/activities.js
+++ api/activities.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { getAllActivities } = require("../db");
+const { getAllActivities, updateActivity, createActivity } = require("../db");
+const requireUser = require('./utils')
 const activitiesRouter = express.Router();
 
 activitiesRouter.get('/', async (req, res, next) => {
@@ -7,6 +8,41 @@ activitiesRouter.get('/', async (req, res, next) => {
         const allActivities = await getAllActivities()
 
         res.send(allActivities)
+    } catch (error) {
+        throw error
+    }
+})
+
+activitiesRouter.post('/', requireUser, async (req, res, next) => {
+    const { name, description } = req.body
+
+    try {
+        const newActivity = await createActivity({ name, description })
+
+        res.send(newActivity)
+    } catch (error) {
+        throw error
+    }
+})
+
+activitiesRouter.patch('/:activityId', requireUser, async (req, res, next) => {
+    const { activityId } = req.params
+    const { name, description } = req.body
+
+    const updateFields = {}
+
+    if (name) {
+        updateFields.name = name
+    }
+
+    if (description) {
+        updateFields.description = description
+    }
+
+    try {
+        const updatedActivity = await updateActivity({ activityId, updateFields })
+
+        res.send(updatedActivity)
     } catch (error) {
         throw error
     }
--- db/activities.js
+++ db/activities.js
@@ -1,7 +1,6 @@
 const client = require("./client");
 
 const createActivity = async ({ name, description }) => {
-  name = name.toLowerCase();
   try {
     const {
       rows: [activity],
@@ -52,10 +51,10 @@ const updateActivity = async ({ id, ...fields }) => {
   try {
 
     const setString = Object.keys(fields)
-    .map((key, index) => `"${key}" =$${index + 1}`)
-    .join(", ");
+      .map((key, index) => `"${key}" =$${index + 1}`)
+      .join(", ");
 
-    const { rows: [activity]} = await client.query(`
+    const { rows: [activity] } = await client.query(`
     UPDATE activities
     SET ${setString}
     WHERE id=${id}
@@ -69,34 +68,34 @@ const updateActivity = async ({ id, ...fields }) => {
 };
 
 async function attachActivitiesToRoutines(routines) {
-    // no side effects
-    const routinesToReturn = [...routines];
-    const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
-    const routineIds = routines.map(routine => routine.id);
-    if (!routineIds?.length) return [];
-    
-    try {
-      // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
-      const { rows: activities } = await client.query(`
+  // no side effects
+  const routinesToReturn = [...routines];
+  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
+  const routineIds = routines.map(routine => routine.id);
+  if (!routineIds?.length) return [];
+
+  try {
+    // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
+    const { rows: activities } = await client.query(`
         SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
         FROM activities 
         JOIN routine_activities ON routine_activities."activityId" = activities.id
-        WHERE routine_activities."routineId" IN (${ binds });
+        WHERE routine_activities."routineId" IN (${binds});
       `, routineIds);
-  
-      // loop over the routines
-      for(const routine of routinesToReturn) {
-        // filter the activities to only include those that have this routineId
-        const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
-        // attach the activities to each single routine
-        routine.activities = activitiesToAdd;
-      }
-      return routinesToReturn;
-      
-    } catch (error) {
-      throw error;
+
+    // loop over the routines
+    for (const routine of routinesToReturn) {
+      // filter the activities to only include those that have this routineId
+      const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
+      // attach the activities to each single routine
+      routine.activities = activitiesToAdd;
     }
+    return routinesToReturn;
+
+  } catch (error) {
+    throw error;
   }
+}
 
 module.exports = {
   createActivity,
